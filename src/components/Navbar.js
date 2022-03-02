9f155a555714ff3689cf0ff66ac5f355043ce23e
Asad Mehboob <asad.mehboob@probaseapps.com>
2022-03-02 18:09:25 +0500

    github changes

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -29,6 +29,7 @@ const Navbar = (props) => {
           >
             <span className="navbar-toggler-icon"></span>
           </button>
+          
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
               <li className="nav-item">
